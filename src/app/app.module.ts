85ffa84e3f1f20c2715778ab35128cc394299c1d
Francisco Javier Jimenez <>
2023-01-25 11:34:06 +0100

    reoorganize persona and formulario in personas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { PersonaComponent } from './persona/persona.component';
-import { FormularioComponent } from './formulario/formulario.component';
+import { PersonaComponent } from './personas/persona/persona.component';
+import { FormularioComponent } from './personas/formulario/formulario.component';
 import { LoggingService } from './LoggingService.service';
 import { PersonasService } from './persona.service';
+import { PersonasComponent } from './personas/personas.component';
 
 
 @NgModule({
   declarations: [ 
     AppComponent,
     PersonaComponent,
-    FormularioComponent
+    FormularioComponent,
+    PersonasComponent
   ],
   imports: [
     BrowserModule,
