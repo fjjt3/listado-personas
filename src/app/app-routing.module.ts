a65103134d0fee8069b89f33889c04cf423f0e6c
Francisco Javier Jimenez <>
2023-02-13 16:35:41 +0100

    error page, get , update and delete person

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
-import { PersonasComponent } from './personas/personas.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 import { FormularioComponent } from './personas/formulario/formulario.component';
+import { PersonasComponent } from './personas/personas.component';
 
 const routes: Routes = [
-  {path:'', component: PersonasComponent},
-  {path: 'personas', component: PersonasComponent},
-  {path: 'personas/agregar', component: FormularioComponent},
-  {path: 'personas/id', component: FormularioComponent}
+  {path: '', component: PersonasComponent},
+  {path: 'personas', component: PersonasComponent, children: [
+    {path: 'agregar', component: FormularioComponent},
+    {path: ':id', component: FormularioComponent}
+  ]},
+  {path: '**', component: ErrorComponent}
 ]
 
 @NgModule({
   imports: [RouterModule.forRoot(
     routes
-  ) 
-  ],
-  exports: [RouterModule]
+  )],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -8,15 +7,17 @@ import { PersonaComponent } from './personas/persona/persona.component';
 import { FormularioComponent } from './personas/formulario/formulario.component';
 import { LoggingService } from './LoggingService.service';
 import { PersonasService } from './persona.service';
+import { AppRoutingModule } from './app-routing.module';
 import { PersonasComponent } from './personas/personas.component';
-
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [ 
     AppComponent,
     PersonaComponent,
     FormularioComponent,
-    PersonasComponent
+    PersonasComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
